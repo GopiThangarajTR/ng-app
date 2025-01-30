b9317ae6fe0fc1ed99c947d306bb40069c91c897
Gopi Thangaraj <gopi.thangaraj@thomsonreuters.com>
2025-01-30 15:51:04 +0530

    Updated lint config & gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'ng-app';
+export class AppComponent implements OnInit {
+  title: any;
+
+  ngOnInit() {
+    this.title = 'ng-app';
+  }
+
+  testTitle(titleVal: string) {
+    console.log('testTitle::', this.title == titleVal);
+  }
 }
